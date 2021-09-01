75fb3aa288a7b67aa376559e89120d7129abbe66
QuinntyneBrown <quinntynebrown@gmail.com>
2021-08-31 23:28:09 -0400

    chore(security): use SHA512

--- src/Rest.Api/Core/PasswordHasher.cs
+++ src/Rest.Api/Core/PasswordHasher.cs
@@ -11,13 +11,11 @@ namespace Rest.Api.Core
     public class PasswordHasher : IPasswordHasher
     {
         public string HashPassword(Byte[] salt, string password)
-        {
-            return Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            => Convert.ToBase64String(KeyDerivation.Pbkdf2(
             password: password,
             salt: salt,
-            prf: KeyDerivationPrf.HMACSHA1,
+            prf: KeyDerivationPrf.HMACSHA512,
             iterationCount: 10000,
             numBytesRequested: 256 / 8));
-        }
     }
 }
